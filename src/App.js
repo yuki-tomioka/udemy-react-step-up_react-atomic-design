12ae0a8b1d9cda265cc76bda8eefc9b54b8db071
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-09-20 23:33:03 +0900

    Pageの作成

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./components/organisms/user/UserCard";
-import { DeafultLayout } from "./components/templates/DeafultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
+import { Router } from "./router/Router";
 
 import "./styles.css";
 
-const user = {
-  name: "test",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "test@example.com",
-  phone: "090-1111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://example.com"
-};
-
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DeafultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DeafultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
