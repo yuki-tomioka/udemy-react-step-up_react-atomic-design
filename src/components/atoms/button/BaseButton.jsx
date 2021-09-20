af43e0a3affe56543254c808c48cfea1852c7e2c
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2021-09-20 21:17:55 +0900

    Atomの作成

---
+++ src/components/atoms/button/BaseButton.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const BaseButton = styled.button`
+  color: #fff;
+  padding: 6px 24px;
+  border: none;
+  border-radius: 2em;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
